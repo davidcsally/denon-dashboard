ab5315cebc3dbe68ae139dcf99d213ef4f591fc4
David Sally <davidcsally@gmail.com>
2018-04-05 23:44:40 -0700

    (add) volume util func

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,19 @@
 /* eslint import/prefer-default-export: 0 */
 export const mute = async () => {
-  console.log('mute');
   try {
     const response = await fetch('http://localhost:2222/volume/mute', {
+      method: 'POST',
+    });
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    return console.log('error: ', e);
+  }
+};
+
+export const volume = async (volume) => {
+  try {
+    const response = await fetch(`http://localhost:2222/volume/${volume}`, {
       method: 'POST',
     });
     const data = await response.json();
